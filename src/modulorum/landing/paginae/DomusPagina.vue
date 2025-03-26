abae11d6cbfa89ea903f25f8f6257fc346e9efa3
Esbosan <esthersanchobosch@gmail.com>
2025-03-26 20:19:36 +0100

    Páginas landingPage

--- src/modulorum/landing/paginae/DomusPagina.vue
+++ src/modulorum/landing/paginae/DomusPagina.vue
@@ -1,1 +1,13 @@
 
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl sm:text-5xl font-bold tracking-tighter">
+    Bienvenido a nuestro sitio web
+    </h1>
+
+    <p class="md:text-xl text-gray-500 mx-auto w-[600px]">
+      Donum mox recipies pro fidelitate erga fructus nostros
+    </p>
+  </div>
+
+</template>
